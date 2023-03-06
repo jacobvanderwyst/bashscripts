3950ac936764f6e3e1d8025f7a48e5a9326c5040
jacobvanderwyst <vjake870@gmail.com>
2023-03-06 14:44:52 -0600

    user agent

--- setup.ps1
+++ setup.ps1
@@ -1,19 +1,20 @@
 #the following command must be used to allow execution of the script
 #Set-ExecutionPolicy RemoteSigned
+#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
 # Install Python
 Write-Host "Installing Python..."
 Start-Process -FilePath "https://www.python.org/ftp/python/3.12.0/python-3.12.0a5.exe" -ArgumentList "/quiet", "InstallAllUsers=1", "PrependPath=1" -Wait
 
 # Install Snort
 Write-Host "Installing Snort..."
-Invoke-WebRequest -Uri "https://www.snort.org/downloads/snort/daq-2.0.7.tar.gz" -OutFile "daq-2.0.7.tar.gz"
+Invoke-WebRequest -Uri "https://www.snort.org/downloads/snort/daq-2.0.7.tar.gz" -OutFile "daq-2.0.7.tar.gz" -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 Expand-Archive -Path "daq-2.0.7.tar.gz" -DestinationPath ".\Snort"
 cd .\Snort\daq-2.0.7
 ./configure
 make
 make install
 cd ..
-Invoke-WebRequest -Uri "https://www.snort.org/downloads/snort/snort-2.9.20.tar.gz" -OutFile "snort-2.9.20.tar.gz"
+Invoke-WebRequest -Uri "https://www.snort.org/downloads/snort/snort-2.9.20.tar.gz" -OutFile "snort-2.9.20.tar.gz" -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 Expand-Archive -Path "snort-2.9.20.tar.gz" -DestinationPath ".\Snort"
 cd .\Snort\snort-2.9.20
 ./configure --enable-sourcefire --enable-inline --prefix=/usr/local/snort
