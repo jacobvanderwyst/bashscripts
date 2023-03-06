cbe8963ef4a9bd1aca4951720628768bb4f4981e
jacobvanderwyst <vjake870@gmail.com>
2023-03-06 16:21:37 -0600

    make me whole

--- setup.ps1
+++ setup.ps1
@@ -17,13 +17,13 @@ Write-Host "Installing Snort..."
 Invoke-WebRequest -Uri "https://www.snort.org/downloads/snort/daq-2.0.7.tar.gz" -OutFile "daq-2.0.7.tar.gz" -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 tar -xf "daq-2.0.7.tar.gz"
 cd daq-2.0.7
-make install
+.\make install
 cd ..
 
 Invoke-WebRequest -Uri "https://www.snort.org/downloads/snort/snort-2.9.20.tar.gz" -OutFile "snort-2.9.20.tar.gz" -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 tar -xf "snort-2.9.20.tar.gz"
 cd .\snort-2.9.20
-make install
+.\make install
 cd ..
 
 # Cleanup
