508591436cbfb112811c295cad8c81f7a12768de
jacobvanderwyst <vjake870@gmail.com>
2023-03-06 16:42:24 -0600

    asfugbasg

--- setup.ps1
+++ setup.ps1
@@ -5,33 +5,21 @@
 # Install Python
 Write-Host "Installing Python..."
 #Start-Process -FilePath "https://www.python.org/ftp/python/3.12.0/python-3.12.0a5.exe" -ArgumentList "/quiet", "InstallAllUsers=1", "PrependPath=1" -Wait
-Read-Host -Prompt "Press any key to continue..."
-
-#install make 
-Write-Host "Installing Make..."
-#~\Desktop\bashscripts\make.exe
+#~\downloads\python-3.12.0a5.exe
 Read-Host -Prompt "Press any key to continue..."
 
 # Install Snort
 Write-Host "Installing Snort..."
-Invoke-WebRequest -Uri "https://www.snort.org/downloads/snort/daq-2.0.7.tar.gz" -OutFile "daq-2.0.7.tar.gz" -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
-tar -xf "daq-2.0.7.tar.gz"
-cd daq-2.0.7
-.\make install
-cd ..
-
-Invoke-WebRequest -Uri "https://www.snort.org/downloads/snort/snort-2.9.20.tar.gz" -OutFile "snort-2.9.20.tar.gz" -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
-tar -xf "snort-2.9.20.tar.gz"
-cd .\snort-2.9.20
-.\make install
-cd ..
+Invoke-WebRequest -Uri "https://www.snort.org/downloads/snort/Snort_2_9_20_Installer.x64.exe" -OutFile "~/downloads/Snort_2_9_20_Installer.x64.exe" -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+#Start-Process -FilePath "https://www.snort.org/downloads/snort/Snort_2_9_20_Installer.x64.exe" -ArgumentList "/quiet", "InstallAllUsers=1", "PrependPath=1" -Wait
+~\downloads\Snort_2_9_20_Installer.x64.exe
+Read-Host -Prompt "Press any key to continue..."
 
 # Cleanup
 Write-Host "Cleaning up..."
-#Remove-Item "daq-2.0.7.tar.gz" -Force
-#Remove-Item "snort-2.9.20.tar.gz" -Force
-#Remove-Item -Recurse ".\Snort" -Force
-#Remove-Item "gzip_installer" -Force
+#Remove-Item "~/downloads/Snort_2_9_20_Installer.x64.exe" -Force
+#Remove-Item "~/downloads/python-3.12.0a5.exe" -Force
+#Remove-Item "" -Force
 
 #disabling script execution
 #Set-ExecutionPolicy Restricted
