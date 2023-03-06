d382ce38351a29f4fbc71e9f04b9517c8da1eac1
jacobvanderwyst <vjake870@gmail.com>
2023-03-06 16:04:53 -0600

    many changes

--- setup.ps1
+++ setup.ps1
@@ -10,10 +10,8 @@ Start-Process -FilePath "https://www.python.org/ftp/python/3.12.0/python-3.12.0a
 Write-Host "Installing Snort..."
 Invoke-WebRequest -Uri "https://www.snort.org/downloads/snort/daq-2.0.7.tar.gz" -OutFile "daq-2.0.7.tar.gz" -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 tar -xf "daq-2.0.7.tar.gz"
-cd .\daq-2.0.7
-./configure
-make
-make install
+cd daq-2.0.7
+~\Desktop\bashscripts\make.exe install.sh  
 cd ..
 Invoke-WebRequest -Uri "https://www.snort.org/downloads/snort/snort-2.9.20.tar.gz" -OutFile "snort-2.9.20.tar.gz" -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 tar -xf "snort-2.9.20.tar.gz"
